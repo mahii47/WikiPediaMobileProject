841bc412993842c2e8ab1b6b790385f3daf7bbc9
mahii47 <mahmankar333@gmail.com>
2025-11-19 20:34:13 +0530

    Basic interrupt test created.

--- src/test/java/negativetests/InterruptTest.java
+++ src/test/java/negativetests/InterruptTest.java
@@ -1,11 +1,23 @@
 package negativetests;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import base.RegressionBaseTest;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import utils.WaitUtil;
 
 public class InterruptTest  extends RegressionBaseTest{
 	
-	  @Test
-	    public void testInternetInterruption() throws Exception {
+	 By languageMenuIcon = By.xpath("//android.widget.TextView[@content-desc=\"Language\"]");
+//	 By retrybutton = Retry;
+//	 By language = Marathi;
+	 
+	 	private WaitUtil waitUtil;
+	 
+		
+	 
+	  @Test(priority=0)
+	    public void turnOnInternetInterruption() throws Exception {
 		  
 	        System.out.println("=== INTERRUPT TEST: MOBILE DATA STARTED ===");
 
@@ -18,8 +30,21 @@ public class InterruptTest  extends RegressionBaseTest{
 	        System.out.println("Mobile Data is now OFF.");
 
 	        // Add any step here (scroll article / click section) to show the impact in video
-	       
-	     
+	  }
+	  
+	  @Test(priority=1)
+	  public void InternetInterruption()
+	  {
+		  MobileElement iconLanguage = driver.findElement(languageMenuIcon);
+				  //waitUtil.waitForClickable(languageMenuIcon);
+		  iconLanguage.click();
+	  }
+	  
+	   
+	  
+	  @Test(priority = 2)
+	    public void turnOffInternetInterruption() throws Exception
+	    {
 	        Thread.sleep(4000);
 
 	        // Turn ON Mobile Data
