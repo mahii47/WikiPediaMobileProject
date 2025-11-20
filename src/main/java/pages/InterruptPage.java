29da5734ef7357a12c149c5d3d067e34004d4479
mahii47 <mahmankar333@gmail.com>
2025-11-20 21:38:56 +0530

    Interrupt test completed.

---
+++ src/main/java/pages/InterruptPage.java
@@ -0,0 +1,74 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import io.qameta.allure.Step;
+import utils.WaitUtil;
+
+public class InterruptPage {
+	
+	    private AndroidDriver<MobileElement> driver;
+	    private WaitUtil waitUtil;
+
+	    public InterruptPage(AndroidDriver<MobileElement> driver) {
+	        this.driver = driver;
+	        this.waitUtil = new WaitUtil(driver);
+	    }
+	     By languageMenuIcon = By.xpath("//android.widget.TextView[@content-desc=\"Language\"]");
+		
+	 	
+		    public void turnOffInternetInterruption() throws Exception {
+				  
+		        System.out.println("=== INTERRUPT TEST: MOBILE DATA STARTED ===");
+
+
+		        // Turn OFF Mobile Data
+		        System.out.println("Turning Mobile Data OFF...");
+		        Runtime.getRuntime().exec("adb shell svc data disable");
+		        Thread.sleep(5000);
+		        System.out.println("Mobile Data is now OFF.");
+		  }
+		    public void InternetInterruption()
+			  {
+				  MobileElement iconLanguage = waitUtil.waitForClickable(languageMenuIcon);
+				  iconLanguage.click();
+				  retryButton();
+			  }
+		    public void turnOnnInternetInterruption() throws Exception
+		    {
+		        Thread.sleep(4000);
+
+		        // Turn ON Mobile Data
+		        System.out.println("Turning Mobile Data ON...");
+		        Runtime.getRuntime().exec("adb shell svc data enable");
+		        Thread.sleep(6000);
+		        System.out.println("Mobile Data is now ON.");
+
+		        System.out.println("=== INTERRUPT TEST COMPLETED ===");
+		  }
+		    public void languageSelection() throws InterruptedException
+			  {
+				  retryButton();
+				  tapLanguageOption();
+			  }
+			  public void retryButton()
+			  {
+				  new TouchAction(driver)
+			        .tap(PointOption.point(540,1501))
+			        .perform();
+				  System.out.println("Retry button clicked!!");
+
+			  }
+			  public void tapLanguageOption() throws InterruptedException {
+				    new TouchAction(driver)
+				            .tap(PointOption.point(540, 1365))
+				            .perform();
+				    System.out.println("Langauge clicked!!");
+				    Thread.sleep(5000);
+				}
+
+}
--- src/test/java/negativetests/InterruptTest.java
+++ src/test/java/negativetests/InterruptTest.java
@@ -4,55 +4,18 @@ import org.testng.annotations.Test;
 import base.RegressionBaseTest;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import pages.InterruptPage;
 import utils.WaitUtil;
 
 public class InterruptTest  extends RegressionBaseTest{
 	
-	 By languageMenuIcon = By.xpath("//android.widget.TextView[@content-desc=\"Language\"]");
-//	 By retrybutton = Retry;
-//	 By language = Marathi;
-	 
-	 	private WaitUtil waitUtil;
-	 
-		
-	 
-	  @Test(priority=0)
-	    public void turnOnInternetInterruption() throws Exception {
-		  
-	        System.out.println("=== INTERRUPT TEST: MOBILE DATA STARTED ===");
-
-	        Thread.sleep(3000);
-
-	        // Turn OFF Mobile Data
-	        System.out.println("Turning Mobile Data OFF...");
-	        Runtime.getRuntime().exec("adb shell svc data disable");
-	        Thread.sleep(5000);
-	        System.out.println("Mobile Data is now OFF.");
-
-	        // Add any step here (scroll article / click section) to show the impact in video
-	  }
-	  
-	  @Test(priority=1)
-	  public void InternetInterruption()
-	  {
-		  MobileElement iconLanguage = driver.findElement(languageMenuIcon);
-				  //waitUtil.waitForClickable(languageMenuIcon);
-		  iconLanguage.click();
-	  }
-	  
-	   
-	  
-	  @Test(priority = 2)
-	    public void turnOffInternetInterruption() throws Exception
-	    {
-	        Thread.sleep(4000);
-
-	        // Turn ON Mobile Data
-	        System.out.println("Turning Mobile Data ON...");
-	        Runtime.getRuntime().exec("adb shell svc data enable");
-	        Thread.sleep(6000);
-	        System.out.println("Mobile Data is now ON.");
-
-	        System.out.println("=== INTERRUPT TEST COMPLETED ===");
-	  }
+	@Test
+	public void InternetInterruption() throws Exception
+	{
+		 InterruptPage page = new InterruptPage(driver);
+		 page.turnOffInternetInterruption();
+		 page.InternetInterruption();
+		 page.turnOnnInternetInterruption();
+		 page.languageSelection();
+	}
 }
